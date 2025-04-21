719fd52c81d4b8f0af03a80c3463e88458fcfed9
ACCASE <61645103+ACCASE@users.noreply.github.com>
2025-04-20 23:12:27 -0500

    Normalization for all algorithms

--- ACCASE/Grouped_Action_DQN/DQN_Basic.py
+++ ACCASE/Grouped_Action_DQN/DQN_Basic.py
@@ -158,6 +158,24 @@ class Agent:
         fig.savefig(self.GRAPH_FILE)
         plt.close(fig)
 
+    def normalize_state(self, state, num_rows, num_cols):
+        """
+        Normalize the state to be between 0 and 1
+        """
+        # Normalize the state by dividing by the maximum value in the state
+        # Build Vector to normalize state by
+        # Column Height and Max Height
+        normalization_vector = torch.ones(1,num_cols+1, device=device) * num_rows
+        # Holes
+        max_holes = num_cols*num_rows
+        normalization_vector = torch.cat((normalization_vector, torch.ones(1,1, device=device) * max_holes), dim=1)
+        # Bumpiness
+        max_bumpiness = num_rows
+        normalization_vector = torch.cat((normalization_vector, torch.ones(1,1, device=device) * max_bumpiness), dim=1)
+        # Normalize the state
+        normalized_state = state / normalization_vector
+        return normalized_state
+
     def run(self, training=True, render=False):
         """
         Train the agent using Deep Q-Learning
@@ -172,6 +190,8 @@ class Agent:
         # Get input and output dimensions
         num_states = env.observation_space.shape[1]
         num_actions = 1 #env.action_space.n
+        num_rows = env.unwrapped.height
+        num_cols = env.unwrapped.width
         
         # Initialize DQN
         policy_net = DQN(num_states, num_actions).to(device)
@@ -199,6 +219,7 @@ class Agent:
         for episode in itertools.count():
             state, info = env.reset()
             state = torch.tensor(state, dtype=torch.float, device=device)
+            state = self.normalize_state(state, num_rows, num_cols) # Normalize state
             env.render()
             terminated = False
             truncated = False
@@ -247,6 +268,7 @@ class Agent:
 
                 # Convert new state and reward to tensors
                 new_state = torch.tensor(new_state, dtype=torch.float, device=device)
+                new_state = self.normalize_state(new_state, num_rows, num_cols) # Normalize state
                 reward = torch.tensor(reward, dtype=torch.float, device=device)
 
                 self.optimize(reward, action, state, new_state, terminated, policy_net)
